a9100c2b98ebcbb4c2c06576fa57999771af6264
Dexter Fournier <defourni@cs.ualberta.ca>
2020-01-08 17:54:21 -0700

    update scrpit to get itself from github and print src code

--- lab1_script.py
+++ lab1_script.py
@@ -2,5 +2,8 @@ import requests
 
 #print(requests.__version__)
 
-google = requests.get("http://www.google.com")
-print(google)
+#google = requests.get("http://www.google.com")
+#print(google)
+script = requests.get("https://raw.githubusercontent.com/defourni/CMPUT404-Lab1/master/lab1_script.py")
+print(script.text)
+
